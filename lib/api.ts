0f75e771276de4ebc4e8e435552cbfcc6967bda7
gab Henrique <gabriel.aquino@camptecnologia.com.br>
2022-09-15 16:40:17 -0300

    fixing postapi

--- lib/api.ts
+++ lib/api.ts
@@ -1,22 +1,17 @@
 import { getCookie, setCookie } from "./cookie";
 
 export async function postApi<JSON = any>(url: string, object: any,): Promise<JSON> {
-    try{
         
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-                'Authorization': `Bearer ${await getCookie('auth')}`,
-            },
-            body: JSON.stringify(object)
-        });
-        const data = await response.json();
-        return data;
-
-    } catch(e){
-        return JSON.parse(e as string)
-    }
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Authorization': `Bearer ${await getCookie('auth')}`,
+        },
+        body: JSON.stringify(object)
+    });
+    const data = await response.json();
+    return data;
   
 }
 
